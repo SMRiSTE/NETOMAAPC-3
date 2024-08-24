d04072dd1b265f9b66392d2de36152f32e28a815
SMRist <s.m.r.00800ist@gmail.com>
2024-08-25 00:43:01 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include<thread>
+#include<future>
+#include<vector>
+#include<algorithm>
+
+auto FindMin(std::vector<int>& fvec, int num) {
+	std::future<int> fut;
+	std::promise<int> p;
+	fut = p.get_future();
+	int min_index = num;
+	for (int i = num + 1; i < fvec.size(); i++) {
+		if (fvec[i]<fvec[min_index]) {
+			min_index = i;
+		}
+	}
+	p.set_value(min_index);
+	return fut;
+}
+
+int main()
+{
+	std::vector<int> vec = { 2,4,3,1,7,5,6,9,8,0 };
+	for(int& n : vec) {
+		std::cout << n << ", ";
+	}
+	std::cout << std::endl;
+	
+	for (int i = 0; i < vec.size(); i++) {
+		std::future<int> f = FindMin(vec,i);
+		int min_index = f.get();
+		std::swap(vec[i], vec[min_index]);
+	}
+	for (int& n : vec) {
+		std::cout << n << ", ";
+	}
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+void For_each(std::vector<int>::iterator begin, std::vector<int>::iterator end, void (*function)(int&)) {
+    for (std::vector<int>::iterator it = begin; it != end; ++it) {
+        function(*it);
+    }
+}
+
+void print(int& num) {
+    std::cout << num << " ";
+}
+
+void divide_container(std::vector<int>& container) {
+    if (container.size() == 1) {
+        std::function<void(int&)> func = print;
+        std::future<void> f = std::async(std::launch::async, For_each, container.begin(), container.end(), &print);
+        f.get();
+        return;
+    }
+    if (container.empty()) {
+        return;
+    }
+
+    int n_size = container.size() / 2;
+    std::vector<int> left, right;
+
+    for (int i = 0; i < n_size; i++) {
+        left.push_back(container[i]);
+    }
+    for (int i = n_size; i < container.size(); i++) {
+        right.push_back(container[i]);
+    }
+
+    divide_container(left);
+    divide_container(right);
+}
+
+int main()
+{
+    std::vector<int> vec = { 1,2,3,4,5,6,7,8,9,0,51,55,32,87,90 };
+
+    divide_container(vec);
+
+    return 0;
+}
